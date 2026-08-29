902dd94ddb475d520b88aa0f3ec7b2d8e2879f37
onyemachichibuzo44-collab <onyemachichibuzo44@gmail.com>
2026-08-29 20:05:04 +0100

    Change metadata title to 'VIS Academy'
    Updated metadata title and description in RootLayout.

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import "./globals.css"‎‎
+‎export const metadata={title:"VIS Academy"}`
+‎export default function RootLayout({children}:{children:any}){return(<html lang="en"><body>{children}</body></html>)}`
